0df24c24d255339e8af152e506e0a1993540e98b
Lewis Gregerson-Spencer <lewisgregerson-spencer@D-10-19-186-87.dhcp4.washington.edu>
2019-01-28 21:19:31 -0800

    first commit

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+export const facebook = {
+    clientID: '760243844344815',
+    clientSecret: 'c39dc7fe064d49a544226619a2f779d3',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+  };
+  
+  export const google = {
+    clientID: '719280382149-j479aohm5ci4kkd91kqaj84r029tpa5u.apps.googleusercontent.com',
+    clientSecret: '4nYyHz6tPwspXTENRmUMML0d',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+  };
